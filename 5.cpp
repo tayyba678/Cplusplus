4d7b3974047af1820f2125d78a8e92b6ad7c7210
Tayyba Aftab <144408026+tayyba678@users.noreply.github.com>
2023-10-13 15:03:07 +0500

    Add files via upload

--- 5.cpp
+++ 5.cpp
@@ -1,12 +1,93 @@
 #include<iostream>
 using namespace std;
-int  main(){
-cout<<"Enter the Charge (Q) in Coulombs: ";
-float Q,T;
-cin>> Q;
-cout<<"Enter the time (T) in seconds: ";
-cin>> T;
-float I;
-I=Q/T;
-cout<<"The current (I) is: "<<I<<" Amperes";
+float calculateFruitPrice(string fruit, string day, double quantity);
+main(){
+    string f,d;
+    float q;
+    float o;
+    cout<<"Fruits: ";
+    cin>> f;
+    cout<<"Day: ";
+    cin >>d;
+    cout<<"Quantity: ";
+    cin>> q;
+    o=calculateFruitPrice(f,d,q);
+    cout<<o;
 }
+float calculateFruitPrice(string fruit, string day, double quantity)
+{
+    float pr,pri;
+    if(fruit=="banana"&&day=="Sunday")
+    {
+        pr=2.70;
+        pri=pr*quantity;
+    }
+     if(fruit=="banana"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=2.50;
+        pri=pr*quantity;
+    }
+    if(fruit=="apple"&&day=="Sunday")
+    {
+        pr=1.25;
+        pri=pr*quantity;
+    }
+     if(fruit=="apple"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=1.20;
+        pri=pr*quantity;
+    }
+    if(fruit=="orange"&&day=="Sunday")
+    {
+        pr=0.90;
+        pri=pr*quantity;
+    }
+     if(fruit=="orange"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=0.85;
+        pri=pr*quantity;
+    }
+    if(fruit=="grapefruit"&&day=="Sunday")
+    {
+        pr=1.60;
+        pri=pr*quantity;
+    }
+     if(fruit=="grapefruit"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=1.45;
+        pri=pr*quantity;
+    }
+    if(fruit=="kiwi"&&day=="Sunday")
+    {
+        pr=3.00;
+        pri=pr*quantity;
+    }
+     if(fruit=="kiwi"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=2.70;
+        pri=pr*quantity;
+    }
+    if(fruit=="pineapple"&&day=="Sunday")
+    {
+        pr=5.60;
+        pri=pr*quantity;
+    }
+     if(fruit=="pineapple"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=5.50;
+        pri=pr*quantity;
+    }
+    if(fruit=="grapes"&&day=="Sunday")
+    {
+        pr=4.20;
+        pri=pr*quantity;
+    }
+     if(fruit=="grapes"&&(day=="Monday"||day=="Tuesday"||day=="Wednesday"||day=="Thursday"||day=="Friday"||day=="Saturday"))
+    {
+        pr=3.85;
+        pri=pr*quantity;
+    }
+
+return pri;
+}
+
---
+++ Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+string calculateHotelPrices(string month, int numberOfStays);
+main()
+{
+    cout<<"Enter the month (May, June, July, August, September, October): ";
+    cin>>m;
+    cout<<"Enter the number of stays: ";
+    cin>>n;
+    calculateHotelPrices(m,n);
+}
+string calculateHotelPrices(string month, int numberOfStays);
+{
+    
+}
